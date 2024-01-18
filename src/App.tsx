d627bacf3b8f0ce3b243d840cadc932b5f49abd5
Wowaxur <wowaxur@yandex.ru>
2024-01-18 18:39:40 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BlockItem from "./components/block-arrow/BlockItem";
+import MainImg from './components/images/mainImg.webp'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import SvoiLoogo from './components/images/logo_2.png'
+import VkLogo from './components/images/vk.svg'
+class App extends React.Component {
+    render() {
+        return (
+            <div className="main">
+            <div className={'Image'}>
+            <img className={'MainImage'} src={MainImg} alt={''}/> </div>
+                <div className={"Name"}>
+                    <h3 className={'NamePerson'}>Алина Гугасян</h3>
+                    <p>Руководитель партнерского отдела   </p>
+                    <p>АН «Свои на юге»</p>
+                    <p> г. Краснодар</p>
+                </div>
+                <BlockItem/>
+                <div className={'ContainerBlock'}>
+                    {/*<div className={'WhatsappWrapper'}>*/}
+                <div className={'BlockContact'}>
+                    <a rel={"noreferrer"} target={"_blank"} href={'https://wa.me/79284082605'} className={"WhatsApp"}>
+                        <WhatsAppIcon className="iconWhatsApp" />
+                        <div>WhatsApp</div>
+                    </a>
+                </div>
+                 {/*   <div className={'BlockContact'}>
+                        <a rel={"noreferrer"} target={"_blank"} href={'https://wa.me/79180892823'} className={"WhatsApp"}>
+                            <WhatsAppIcon
+                                className="iconWhatsApp" />
+                            <div>Личный</div>
+                        </a>
+                    </div>
+                    </div>*/}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                    <div className={'BlockContact'}>
+                    <a rel={"noreferrer"} href={'https://www.instagram.com/svoi.live/'} target={"_blank"} className={"Instagram"}>
+                        <InstagramIcon className={'iconInst'}/>
+                        Instagram</a>
+                </div>
+                    <div className={'BlockContact'}>
+                        <a rel={"noreferrer"}  href={'https://t.me/+4YItH4K3uuk5NTk6'} target={"_blank"} className={"Telegram"}>
+                            <TelegramIcon className={'iconTg'}/>
+                            Telegram</a>
+                    </div>
+                {/*    <div className={'BlockContact'}>
+                    <a rel={"noreferrer"} href={'https://vk.com/svoi.live'} className={"Link"} target={"_blank"}> <img src={VkLogo} alt={''} className={'vkLogo'}/> </a>
+                </div>*/}
+                    <div className={'BlockContact'}>
+                        <a rel={"noreferrer"} href={'https://svoi.live'} className={"Link"} target={"_blank"}> <img className={'SvoiLogo'} alt={''} src={SvoiLoogo} />
+                      </a>
+                    </div>
+
+                </div>
+                <footer className={'footer'}>Агентство недвижимости «Свои на юге»
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
